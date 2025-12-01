21d0b2800c32067def8053e212c450ed77cec881
mxmpl <44207491+mxmpl@users.noreply.github.com>
2025-12-01 16:12:11 +0100

    fix

--- prepare/src/discophon/prepare/__main__.py
+++ prepare/src/discophon/prepare/__main__.py
@@ -3,15 +3,23 @@ from pathlib import Path
 
 from discophon.core import COMMONVOICE_TO_ISO6393
 
-from .core import download_benchmark
-from .prepare import prepare_downloaded_benchmark
+from .core import download_benchmark, prepare_downloaded_benchmark
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest="command", required=True, help="command to run")
-    parser_download = subparsers.add_parser("download", help="download benchmark data")
+    subparsers = parser.add_subparsers(
+        description="Prepare Phoneme Discovery benchmark",
+        dest="command",
+        required=True,
+        help="command to run",
+    )
+    parser_download = subparsers.add_parser(
+        "download",
+        description="Download benchmark data",
+        help="download benchmark data",
+    )
     parser_download.add_argument("data", help="path to data directory", type=Path)
-    parser_audio = subparsers.add_parser("audio", help="prepare audio files")
+    parser_audio = subparsers.add_parser("audio", description="Prepare audio files", help="prepare audio files")
     parser_audio.add_argument("data", help="path to data directory", type=Path)
     parser_audio.add_argument("code", help="CommonVoice language code", type=str, choices=list(COMMONVOICE_TO_ISO6393))
     args = parser.parse_args()
