00436ff0158ed50b217f2c7c3c515b9c809b0114
mxmpl <44207491+mxmpl@users.noreply.github.com>
2025-12-11 17:25:04 +0100

    update

--- core/src/discophon/core/languages.py
+++ core/src/discophon/core/languages.py
@@ -1,37 +1,38 @@
 from enum import StrEnum
 from typing import Literal
 
+COMMONVOICE_TO_ISO639_3 = {
+    "eu": "eus",
+    "ja": "jpn",
+    "sw": "swa",
+    "ta": "tam",
+    "th": "tha",
+    "tr": "tur",
+    "uk": "ukr",
+    "zh-CN": "cmn",
+}
+
 
 class Language(StrEnum):
     # Dev languages
-    UKR = "ukr"  # Ukrainian
-    THA = "tha"  # Thai
-    TUR = "tur"  # Turkish
+    DEU = "deu"  # German
     SWA = "swa"  # Swahili
     TAM = "tam"  # Tamil
-    DEU = "deu"  # German
+    THA = "tha"  # Thai
+    TUR = "tur"  # Turkish
+    UKR = "ukr"  # Ukrainian
+
     # Test languages
-    ENG = "eng"  # English
-    FRA = "fra"  # French
     CMN = "cmn"  # Mandarin Chinese
+    ENG = "eng"  # English
+    EUS = "eus"  # Basque
+    FRA = "fra"  # French
     JPN = "jpn"  # Japanese
     WOL = "wol"  # Wolof
-    EUS = "eus"  # Basque
 
     @classmethod
     def from_commonvoice(cls, code: str) -> "Language":
-        return {
-            "de": cls.DEU,
-            "en": cls.ENG,
-            "fr": cls.FRA,
-            "ja": cls.JPN,
-            "sw": cls.SWA,
-            "ta": cls.TAM,
-            "th": cls.THA,
-            "tr": cls.TUR,
-            "uk": cls.UKR,
-            "zh-CN": cls.CMN,
-        }[code]
+        return cls(COMMONVOICE_TO_ISO639_3[code])
 
     @property
     def split(self) -> Literal["dev", "test"]:
