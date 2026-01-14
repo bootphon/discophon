0f03d485ad7ac8b22914224b49edc4673d7e1bbd
mxmpl <44207491+mxmpl@users.noreply.github.com>
2026-01-14 17:36:31 +0100

    finetune hubert

--- src/discophon/baselines/finetune_hubert.py
+++ src/discophon/baselines/finetune_hubert.py
@@ -1,21 +1,31 @@
 """Training loop."""
 
+import logging
 import math
 from contextlib import ExitStack
 from pathlib import Path
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 
+import joblib
+import polars as pl
 import torch
 import wandb
 from minimal_hubert import HuBERTPretrain
 from minimal_hubert.data import build_dataloader_with_labels
+from minimal_hubert.features import compute_and_save_hubert_features
+from minimal_hubert.kmeans import build_kmeans
+from sklearn.cluster import MiniBatchKMeans
 from spidr.checkpoint import Checkpointer
 from spidr.config import DataConfig, MaskingConfig
+from spidr.data import read_manifest
 from spidr.environment import set_seed, setup_environment, setup_pytorch
 from spidr.tools import AverageMeters, profiler_context
 from torch import GradScaler
 from torch.nn.utils import clip_grad_norm_
 from torch.optim import AdamW, Optimizer, lr_scheduler
 from tqdm import tqdm
+
+logger = logging.getLogger()
 
 
 def tristage_scheduler(
@@ -46,7 +56,60 @@ def hubert_ft_data_config(manifest: str) -> DataConfig:
     )
 
 
-def finetune_hubert(name: str, project: str, workdir: Path, checkpoint: Path, manifest: str) -> None:
+def patch_manifest_with_paths(src: str | Path, dest: str | Path) -> None:
+    manifest = read_manifest(src)
+    if "path" not in manifest.columns:
+        discophon = Path(src).parent.parent.resolve()
+        _, lang, *split = Path(src).stem.split("-")
+        audios = discophon / "audio" / lang / "-".join(split)
+        assert audios.is_dir()
+        manifest = manifest.with_columns(path=pl.concat_str(pl.lit(str(audios) + "/"), "fileid", pl.lit(".wav")))
+    manifest.write_csv(dest)
+
+
+def patch_manifest_with_units(
+    src: str | Path,
+    dest: str | Path,
+    root_features: str | Path,
+    kmeans: MiniBatchKMeans,
+) -> None:
+    root = Path(root_features)
+    manifest = read_manifest(src)
+    new_manifest = []
+    for row in tqdm(manifest.iter_rows(named=True), total=manifest.height):
+        features = torch.load(root / f"{row['fileid']}.pt")
+        units = kmeans.predict(features.numpy()).tolist()
+        new_manifest.append(row | {"units": units})
+    pl.DataFrame(new_manifest).write_ndjson(dest)
+
+
+def fit_kmeans_from_checkpoint(
+    manifest: str,
+    checkpoint: str | Path,
+    root_features: str | Path,
+    layer: int,
+    n_clusters: int,
+    seed: int,
+) -> MiniBatchKMeans:
+    compute_and_save_hubert_features(manifest, root_features, checkpoint, layer)
+    kmeans = build_kmeans(n_clusters, seed=seed)
+    features = torch.concat([torch.load(p) for p in Path(root_features).rglob("*.pt")])
+    kmeans.fit(features)
+    inertia = -kmeans.score(features) / len(features)
+    logger.info("K-means inertia: %s", inertia)
+    return kmeans
+
+
+def finetune_hubert(
+    name: str,
+    project: str,
+    workdir: Path,
+    checkpoint: Path,
+    manifest: str,
+    *,
+    n_clusters: int,
+    target_layer: int,
+) -> None:
     max_steps, seed = 20_000, 0
     with ExitStack() as stack:
         set_seed(seed)
@@ -54,6 +117,14 @@ def finetune_hubert(name: str, project: str, workdir: Path, checkpoint: Path, ma
         setup_environment()
         rundir = workdir / project / name
         rundir.mkdir(parents=True, exist_ok=True)
+        temp_manifest = stack.enter_context(NamedTemporaryFile(suffix=".jsonl"))
+        temp_features = stack.enter_context(TemporaryDirectory(prefix="features-", dir=rundir))
+        patch_manifest_with_paths(manifest, temp_manifest.name)
+        kmeans = fit_kmeans_from_checkpoint(manifest, checkpoint, temp_features.name, target_layer, n_clusters, seed)
+        joblib.dump(kmeans, rundir / "kmeans.joblib")
+        new_manifest = rundir / "manifest-with-units.jsonl"
+        patch_manifest_with_units(temp_manifest.name, new_manifest, temp_features.name, kmeans)
+
         wandb.init(project=project, name=name, mode="offline", dir=workdir)
         stack.callback(wandb.finish)
         device = torch.device("cuda")
@@ -64,7 +135,7 @@ def finetune_hubert(name: str, project: str, workdir: Path, checkpoint: Path, ma
         optimizer = AdamW(model.parameters(), lr=5e-5, weight_decay=0.01, betas=(0.9, 0.95), eps=1e-6, fused=True)
         scaler = GradScaler("cuda")
         scheduler = tristage_scheduler(optimizer, warmup_steps=600, hold_steps=9_400, decay_steps=10_000)
-        loader = build_dataloader_with_labels(hubert_ft_data_config(manifest), MaskingConfig())
+        loader = build_dataloader_with_labels(hubert_ft_data_config(str(new_manifest)), MaskingConfig())
         ckpt = Checkpointer(rundir, 1_000)
         ckpt.init_state(model=model, optimizer=optimizer, scheduler=scheduler, scaler=scaler)
         step, epoch = int(ckpt.step), int(ckpt.epoch)
@@ -125,5 +196,16 @@ if __name__ == "__main__":
     parser.add_argument("workdir", type=Path)
     parser.add_argument("checkpoint", type=Path)
     parser.add_argument("manifest", type=str)
+    parser.add_argument("--n-clusters", type=int, required=True)
+    parser.add_argument("--layer", type=int, required=True)
     args = parser.parse_args()
-    finetune_hubert(args.name, args.project, args.workdir, args.checkpoint, args.manifest)
+
+    finetune_hubert(
+        args.name,
+        args.project,
+        args.workdir,
+        args.checkpoint,
+        args.manifest,
+        n_clusters=args.n_clusters,
+        target_layer=args.layer,
+    )
